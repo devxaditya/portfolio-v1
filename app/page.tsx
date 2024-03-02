c2600e79cbb90df8438a4020ddec06ba62481ee5
Aditya Singh <devxaditya@gmail.com>
2024-03-02 12:30:50 +0530

    Initial Scaffolding

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>UNDER CONSTRUCTION </h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-6xl">UNDER CONSTRUCTION 🏗</h1>
     </main>
   );
 }
